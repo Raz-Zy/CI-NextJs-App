74df056386638e57fe93b8b7f5cb27b79ea764c7
Tan Dara <rathekill007@gmail.com>
2026-09-03 11:27:20 +0700

    update UI

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">This is the Application Version 1.0</h1>
+      <h1 className="text-3xl font-bold">This is the Application Version 2.0</h1>
     </main>
   );
 }
